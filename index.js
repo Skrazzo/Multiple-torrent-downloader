d61592c3302d5b447e1b6017c348eb7aec696989
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-11-25 16:10:26 +0200

    Updated for deno

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 // puppeteer-extra is a drop-in replacement for puppeteer,
 // it augments the installed puppeteer with plugin functionality
-const puppeteer = require("puppeteer-extra");
-// const pLimit = require("p-limit");
-import pLimit from "p-limit";
+import puppeteer from "npm:puppeteer-extra";
+import pLimit from "npm:p-limit";
 
 // Add adblocker plugin, which will transparently block ads in all pages you
 // create using puppeteer.
-const { DEFAULT_INTERCEPT_RESOLUTION_PRIORITY } = require("puppeteer");
-const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
-const { execCommand } = require("./execCommand");
+import { DEFAULT_INTERCEPT_RESOLUTION_PRIORITY } from "npm:puppeteer";
+import AdblockerPlugin from "npm:puppeteer-extra-plugin-adblocker";
+import { execCommand } from "./execCommand.js";
 
 // at the end magnet link will get added
 const aria2cCommand = "aria2c --bt-metadata-only=true --bt-save-metadata=true ";
 
-process.on("SIGINT", function () {
-    console.log("Interrupt signal");
-    process.exit();
-});
 
 (async () => {
     puppeteer.use(
@@ -68,14 +63,14 @@ async function executeDownloading(browser) {
     const pages = await browser.pages();
 
     // get magnet links with function
-    let magnets = await getMagnetLinks(pages);
+    const magnets = await getMagnetLinks(pages);
 
     // Download torrent files
     await downloadAll(magnets);
-    process.exit();
+    Deno.exit();
 }
 
-async function downloadAll(urls, concurrency = 2) {
+function downloadAll(urls, concurrency = 2) {
     const limit = pLimit(concurrency);
     const promises = urls.map((url, _idx) =>
         limit(() => execCommand(aria2cCommand + `"${url}"`))
@@ -84,16 +79,16 @@ async function downloadAll(urls, concurrency = 2) {
 }
 
 async function getMagnetLinks(pages) {
-    let magnets = [];
+    const magnets = [];
 
-    for (let page of pages) {
-        let url = await page.url();
+    for (const page of pages) {
+        const url = await page.url();
 
         if (!url.includes("thepiratebay.org/description.php?id=")) {
             continue;
         }
 
-        let magnet = await page.evaluate(() => {
+        const magnet = await page.evaluate(() => {
             const links = document.querySelectorAll("#d>a");
             let link = "";
 
